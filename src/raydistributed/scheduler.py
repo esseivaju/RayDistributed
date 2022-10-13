6216ff011d8357920b09014f06914ae1b6e9a5c8
Julien Esseiva <esseivaju@gmail.com>
2022-10-12 17:32:19 -0700

    parallelize work distribution

--- src/raydistributed/scheduler.py
+++ src/raydistributed/scheduler.py
@@ -1,6 +1,6 @@
 
 import ray
-
+from ray.util.scheduling_strategies import NodeAffinitySchedulingStrategy
 from raydistributed.algorithms import AlgorithmA, AlgorithmB, AlgorithmC, AlgorithmD, AlgorithmE, StatusCode
 
 
@@ -29,6 +29,22 @@ def bind_dependencies(algo_name, dependencies, algos_map, scheduled_nodes):
     return execute.bind(algos_map[algo_name], *results)
 
 
+@ray.remote
+def schedule_work(algo_deps, algos, requested, nevents):
+    requested_nodes = []
+    scheduled_nodes = {}
+    for algo_name in requested:
+        if algo_name not in scheduled_nodes:
+            scheduled_nodes[algo_name] = bind_dependencies(algo_name, algo_deps, algos, scheduled_nodes)
+        requested_nodes.append(scheduled_nodes[algo_name])
+    # combine the requested nodes
+    dag = sink.bind(*requested_nodes)
+    futures = [None] * nevents
+    for i in range(nevents):
+        futures[i] = dag.execute()
+    return futures
+
+
 class Scheduler:
 
     def __init__(self, algorithms_deps) -> None:
@@ -39,24 +55,26 @@ class Scheduler:
             algo.initialize()
             algos_map[algo.get_name()] = algo
         self._algos = algos_map
-        self._dags = {}
 
     def schedule_n_event(self, nevents, requested):
-        requested_hash = hash(frozenset(requested))
-        futures = [None] * nevents
-        if requested_hash not in self._dags:
-            requested_nodes = []
-            scheduled_nodes = {}
-            for algo_name in requested:
-                if algo_name not in scheduled_nodes:
-                    scheduled_nodes[algo_name] = bind_dependencies(algo_name, self._algorithms_deps, self._algos, scheduled_nodes)
-                requested_nodes.append(scheduled_nodes[algo_name])
-            # combine the requested nodes
-            dag = sink.bind(*requested_nodes)
-            self._dags[requested_hash] = dag
-        else:
-            dag = self._dags[requested_hash]
-
-        for i in range(nevents):
-            futures[i] = dag.execute()
-        return futures
+        nthreads = int(ray.nodes()[0]['Resources']['CPU'])
+        events_per_thread = nevents // nthreads
+        rem = nevents % nthreads
+        futures = [None] * nthreads
+        this = ray.get_runtime_context().node_id
+        for i in range(nthreads):
+            extra = 0
+            if i < rem:
+                extra = 1
+            if not extra and not events_per_thread:
+                break
+            futures[i] = schedule_work.options(
+                                    scheduling_strategy=NodeAffinitySchedulingStrategy(
+                                        node_id=this,
+                                        soft=True)).remote(self._algorithms_deps, self._algos, requested, events_per_thread + extra)
+        futures = [e for e in futures if e is not None]
+        scheduled_work = ray.get(futures)
+        res = []
+        for batch in scheduled_work:
+            res.extend(batch)
+        return res
